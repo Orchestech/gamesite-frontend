689411fa7d1977c24e93d228e970224f1e15b1fd
Rudolf31 <rudolfshevchenko@gmail.com>
2024-03-19 11:29:29 +0300

    Now spirits have a description

--- src/components/MainCarousel.js
+++ src/components/MainCarousel.js
@@ -34,7 +34,7 @@ function Courusel () {
     <CarouselProvider
             // Set initial slide dimensions
             naturalSlideWidth={isMobile ? 10 : 8}
-            naturalSlideHeight={isMobile ? 10 : 3}
+            naturalSlideHeight={isMobile ? 10 : 3.2}
             className="carousel-container" // Add a class for styling
             totalSlides={6} // The number of slides in the carousel
 
@@ -42,34 +42,65 @@ function Courusel () {
         >
         <Slider>
             <Slide index={0} className='about-game__item about-game__item_first'>
-                <img src={Images.Spirit1} className={"img_pixel"} alt='' height={isMobile ? '300px' : '400px'} />
+                <div className='slide'>
+                    <img src={Images.Spirit1} className={"img_pixel"} alt='' height={isMobile ? '300px' : '400px'} />
+                    <h3>Призрак</h3>
+                    <p> Это твой самый распространенный противник</p>
+                    <p> Никаких особенностей, они берут количеством</p>
+                </div>
             </Slide>
             <Slide index={1} className='about-game__item'>
-                <img src={Images.Spirit2} className={"img_pixel"} alt='' height={isMobile ? '300px' : '400px'} />
+                <div className='slide'>
+                    <img src={Images.Spirit2} className={"img_pixel"} alt='' height={isMobile ? '300px' : '400px'} />
+                    <h3>Бешеный маньяк</h3>
+                    <p> Будь всегда на чеку</p>
+                    <p> Их низкая живучесть оправдана безумной скоростью</p>
+                </div>
             </Slide>
             <Slide index={2} className='about-game__item'>
-                <img src={Images.Spirit3} className={"img_pixel"} alt='' height={isMobile ? '300px' : '400px'} />
+                <div className='slide'>
+                    <img src={Images.Spirit6} className={"img_pixel"} alt='' height={isMobile ? '300px' : '400px'} />
+                    <h3>Создание из плазмы</h3>
+                    <p>Надеюсь ты взял с собой зонтик</p>
+                    <p>Постарайся увернуться от их уничтожительных плевков эктоплазмы</p>
+                </div>
             </Slide>
             <Slide index={3} className='about-game__item about-game__item_first'>
-                <img src={Images.Spirit4} className={"img_pixel"} alt='' height={isMobile ? '300px' : '400px'} />
+                <div className='slide'>
+                    <img src={Images.Spirit4} className={"img_pixel"} alt='' height={isMobile ? '300px' : '400px'} />
+                    <h3>Бронированный призрак</h3>
+                    <p>Паро взять калибр покрупнее</p>
+                    <p>Да, они медлительны, но тебе придется постараться, чтоб точно разделаться с ними</p>
+                </div>
             </Slide>
             <Slide index={4} className='about-game__item'>
-                <img src={Images.Spirit5} className={"img_pixel"} alt='' height={isMobile ? '300px' : '400px'} />
+                <div className='slide'>
+                    <img src={Images.Spirit5} className={"img_pixel"} alt='' height={isMobile ? '300px' : '400px'} />
+                    <h3>Неминуемый</h3>
+                    <p>Постарайся внимательно вглядываться в тьму</p>
+                    <p>Подобных созданий ты сможешь заметить только вблизи</p>
+                </div>
             </Slide>
             <Slide index={5} className='about-game__item'>
-                <img src={Images.Spirit6} className={"img_pixel"} alt='' height={isMobile ? '300px' : '400px'} />
+                <div className='slide'>
+                    <img src={Images.Spirit3} className={"img_pixel"} alt='' height={isMobile ? '300px' : '400px'} />
+                    <h3>Смерть</h3>
+                    <p>Сама Смерть с косой уже вышла по твою душу</p>
+                    <p>Это твой главнный противник, удачи тебе</p>
+                </div>
             </Slide>
         </Slider>
         {!isMobile && (
             <>
-                <ButtonBack className="carousel-button">
-                    Предыдущий
-                </ButtonBack>
-                <ButtonNext className="carousel-button">
-                    Следующий
-                </ButtonNext>
+            <ButtonBack className="carousel-button">
+                Предыдущий
+            </ButtonBack>
+            <ButtonNext className="carousel-button">
+                Следующий
+            </ButtonNext>
             </>
         )}
-      </CarouselProvider>
-    );
-}export default Courusel;
+          </CarouselProvider>
+    );}
+
+export default Courusel;
